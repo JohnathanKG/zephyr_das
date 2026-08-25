530d457629f3638a6044cda5834dfbf9e2fb5df9
vi <johnathankoppang@gmail.com>
2026-08-25 00:16:57 -0500

    Reorganize

--- include/das_mcu.h
+++ include/das_mcu.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stddef.h>
+#include <stdarg.h>
 #include <new>
 #include <zephyr/sys/printk.h>
 
@@ -12,6 +13,7 @@ inline void operator delete(void *, void *) noexcept {}
 #define DAS_API
 #define __forceinline inline
 #define DAS_ASSERT(x) ((void)0)
+#define DAS_COMMENT(...)
 #define DAS_BIND_ENUM_CAST(enum_name)
 #define DAS_BIND_ENUM_CAST_98(enum_name)
 #define DAS_BIND_ENUM_CAST_98_IN_NAMESPACE(enum_name, stripped_enum_name)
@@ -79,14 +81,14 @@ struct unordered_map {
 		V second{};
 		bool used = false;
 	};
-	mutable Node nodes[32]{};
+	mutable Node nodes[64]{};
 	struct iterator {
 		Node *p = nullptr;
 		Node *operator->() const { return p; }
 		bool operator!=(iterator o) const { return p != o.p; }
 		bool operator==(iterator o) const { return p == o.p; }
 	};
-	iterator end() const { return {const_cast<Node *>(nodes + 32)}; }
+		iterator end() const { return {const_cast<Node *>(nodes + 64)}; }
 	iterator find(const K &k) const
 	{
 		for (auto &x : nodes)
@@ -175,7 +177,62 @@ struct TypeSize<void> {
 	enum { size = 0 };
 };
 
-enum Type : int32_t { none, tInt = 17, tVoid = 9, tString = 33, tHandle = 51, tPointer = 65 };
+enum Type : int32_t {
+	none,
+	autoinfer,
+	alias,
+	option,
+	typeDecl,
+	typeMacro,
+	fakeContext,
+	fakeLineInfo,
+	anyArgument,
+	tVoid,
+	tBool,
+	tInt8,
+	tUInt8,
+	tInt16,
+	tUInt16,
+	tInt64,
+	tUInt64,
+	tInt,
+	tInt2,
+	tInt3,
+	tInt4,
+	tUInt,
+	tUInt2,
+	tUInt3,
+	tUInt4,
+	tFloat,
+	tFloat2,
+	tFloat3,
+	tFloat4,
+	tDouble,
+	tRange,
+	tURange,
+	tRange64,
+	tURange64,
+	tString,
+	tStructure,
+	tHandle,
+	tEnumeration,
+	tEnumeration8,
+	tEnumeration16,
+	tEnumeration64,
+	tBitfield,
+	tBitfield8,
+	tBitfield16,
+	tBitfield64,
+	tPointer,
+	tFunction,
+	tLambda,
+	tIterator,
+	tArray,
+	tTable,
+	tBlock,
+	tTuple,
+	tVariant
+};
 
 struct Annotation;
 struct StructInfo;
@@ -231,6 +288,29 @@ struct VarInfo : TypeInfo {
 		uint32_t sz, uint64_t h, const char *n, uint32_t off, uint32_t next_gc)
 		: TypeInfo(t, st, et, an, a, b, at, anm, ac, ds, d, fl, sz, h), name(n), offset(off),
 		  nextGcField(next_gc)
+	{
+	}
+};
+
+struct StructInfo {
+	const char *name = nullptr;
+	const char *module_name = nullptr;
+	VarInfo **fields = nullptr;
+	AnnotationInfo *annotations = nullptr;
+	uint64_t hash = 0;
+	uint64_t init_mnh = 0;
+	uint32_t flags = 0;
+	uint32_t count = 0;
+	uint32_t size = 0;
+	uint32_t firstGcField = 0;
+	uint32_t annotation_count = 0;
+	StructInfo() = default;
+	StructInfo(const char *_name, const char *_module_name, uint32_t _flags, VarInfo **_fields,
+		   uint32_t _count, uint32_t _size, uint64_t _init_mnh, AnnotationInfo *_annotations,
+		   uint32_t _annotation_count, uint64_t _hash, uint32_t _firstGcField)
+		: name(_name), module_name(_module_name), fields(_fields), annotations(_annotations),
+		  hash(_hash), init_mnh(_init_mnh), flags(_flags), count(_count), size(_size),
+		  firstGcField(_firstGcField), annotation_count(_annotation_count)
 	{
 	}
 };
@@ -304,10 +384,47 @@ struct SimNode_WithErrorMessage : SimNode {
 struct SimNode_CallBase : SimNode_WithErrorMessage {
 	SimNode_CallBase(const LineInfo &at, const char *msg) : SimNode_WithErrorMessage(at, msg) {}
 	void *aotFunction = nullptr;
+	TypeInfo **types = nullptr;
+	int32_t nArguments = 0;
 };
 
+struct Func {
+	SimFunction *PTR = nullptr;
+	Func() = default;
+	Func(SimFunction *p) : PTR(p) {}
+	explicit operator bool() const { return PTR != nullptr; }
+};
+
+struct Lambda {
+	char *capture = nullptr;
+	Lambda() = default;
+	Lambda(void *ptr) : capture((char *)ptr) {}
+	operator void *() const { return capture; }
+	bool operator==(void *ptr) const { return capture == ptr; }
+	bool operator!=(void *ptr) const { return capture != ptr; }
+};
+
+struct Array {
+	char *data = nullptr;
+	uint64_t size = 0;
+	uint64_t capacity = 0;
+	uint32_t magic = 0;
+	uint32_t lock = 0;
+	uint32_t flags = 0;
+};
+
+struct range {
+	int32_t from = 0;
+	int32_t to = 0;
+	range() = default;
+	explicit range(int32_t t) : from(0), to(t) {}
+	range(int32_t f, int32_t t) : from(f), to(t) {}
+};
+
+inline range mk_range(int32_t i) { return range(i); }
+
 struct NodeAllocator {
-	char buf[2048]{};
+	char buf[16384]{};
 	size_t used = 0;
 	char *allocate(uint32_t n)
 	{
@@ -393,10 +510,49 @@ public:
 		auto it = tabGMnLookup->find(mnh);
 		return it != tabGMnLookup->end() ? it->second : 0;
 	}
+	SimFunction *fnByMangledName(uint64_t mnh)
+	{
+		if (!mnh) return nullptr;
+		auto it = tabMnLookup->find(mnh);
+		return it != tabMnLookup->end() ? it->second : nullptr;
+	}
 	vec4f *abiArguments() { return abiArg; }
+
+	char *allocate(uint32_t n)
+	{
+		if (!n) return nullptr;
+		return new char[n];
+	}
+	void free(char *p, uint32_t) { delete[] p; }
+
+	void throw_error(const char *msg)
+	{
+		printk("das error: %s\n", msg ? msg : "");
+		for (;;) {
+		}
+	}
+	void throw_error_at(const LineInfo *, const char *fmt, ...)
+	{
+		char buf[128];
+		va_list ap;
+		va_start(ap, fmt);
+		vsnprintk(buf, sizeof(buf), fmt, ap);
+		va_end(ap);
+		throw_error(buf);
+	}
+	void throw_error_ex(const char *fmt, ...)
+	{
+		char buf[128];
+		va_list ap;
+		va_start(ap, fmt);
+		vsnprintk(buf, sizeof(buf), fmt, ap);
+		va_end(ap);
+		throw_error(buf);
+	}
 
 	vec4f abiArgBuf[8]{};
 	vec4f *abiArg = abiArgBuf;
+	void *abiCMRES = nullptr;
 	shared_ptr<NodeAllocator> code;
 	char *globals = nullptr;
 	GlobalVariable *globalVariables = nullptr;
@@ -414,11 +570,139 @@ inline void builtin_print(char *text, Context *, LineInfoArg *)
 	printk("%s", text ? text : "");
 }
 
+inline void das_assert(bool cond, Context *ctx)
+{
+	if (!cond) ctx->throw_error("assert failed");
+}
+
+template <typename TT>
+struct TArray : Array {
+	TArray() = default;
+	TArray(TArray &arr) { moveA(arr); }
+	TArray(TArray &&arr) { moveA(arr); }
+	TArray &operator=(TArray &arr)
+	{
+		moveA(arr);
+		return *this;
+	}
+	TArray &operator=(TArray &&arr)
+	{
+		moveA(arr);
+		return *this;
+	}
+	void moveA(TArray &arr)
+	{
+		data = arr.data;
+		arr.data = nullptr;
+		size = arr.size;
+		arr.size = 0;
+		capacity = arr.capacity;
+		arr.capacity = 0;
+		lock = arr.lock;
+		arr.lock = 0;
+		magic = arr.magic;
+		arr.magic = 0;
+		flags = arr.flags;
+		arr.flags = 0;
+	}
+	TT &operator()(int32_t index, Context *ctx)
+	{
+		if (index < 0 || uint64_t(index) >= size)
+			ctx->throw_error_ex("array index out of range, %d of %llu", index,
+					    (unsigned long long)size);
+		return ((TT *)data)[index];
+	}
+	const TT &operator()(int32_t index, Context *ctx) const
+	{
+		if (index < 0 || uint64_t(index) >= size)
+			ctx->throw_error_ex("array index out of range, %d of %llu", index,
+					    (unsigned long long)size);
+		return ((const TT *)data)[index];
+	}
+};
+
+inline void array_lock(Context &, Array &arr, LineInfo *) { ++arr.lock; }
+inline void array_unlock(Context &, Array &arr, LineInfo *)
+{
+	if (arr.lock) --arr.lock;
+}
+
+inline int builtin_array_size(const Array &arr) { return int(arr.size); }
+
+inline void builtin_array_reserve(Array &arr, int newSize, int stride, Context *ctx, LineInfoArg *)
+{
+	if (newSize <= 0 || uint64_t(newSize) <= arr.capacity) return;
+	char *nd = ctx->allocate(uint32_t(uint64_t(newSize) * uint32_t(stride)));
+	if (arr.data) {
+		memcpy(nd, arr.data, size_t(arr.size) * size_t(stride));
+		ctx->free(arr.data, uint32_t(arr.capacity * uint32_t(stride)));
+	}
+	arr.data = nd;
+	arr.capacity = uint64_t(newSize);
+}
+
+inline void builtin_array_resize(Array &arr, int newSize, int stride, Context *ctx, LineInfoArg *at)
+{
+	if (newSize < 0) newSize = 0;
+	if (uint64_t(newSize) > arr.capacity)
+		builtin_array_reserve(arr, newSize, stride, ctx, at);
+	if (uint64_t(newSize) > arr.size && arr.data)
+		memset(arr.data + arr.size * uint32_t(stride), 0,
+		       size_t(uint64_t(newSize) - arr.size) * size_t(stride));
+	arr.size = uint64_t(newSize);
+}
+
 template <typename TT>
 struct das_auto_cast {
 	template <typename QQ>
-	static TT cast(QQ &&e) { return (TT)e; }
+	static TT cast(QQ &&e)
+	{
+		return (TT)e;
+	}
 };
+
+template <typename TT>
+struct das_auto_cast_ref {
+	template <typename QQ>
+	static TT &cast(QQ &e)
+	{
+		return e;
+	}
+	template <typename QQ>
+	static const TT &cast(const QQ &e)
+	{
+		return (const TT &)e;
+	}
+};
+
+template <typename TT>
+struct das_auto_cast_move {
+	template <typename QQ>
+	static TT cast(QQ &&e)
+	{
+		TT res = e;
+		memset((void *)&e, 0, sizeof(QQ));
+		return res;
+	}
+};
+
+template <typename TT>
+struct das_auto_cast_move<TT &> {
+	template <typename QQ>
+	static TT &cast(QQ &e)
+	{
+		return e;
+	}
+};
+
+template <typename LT, typename RT>
+struct das_ordered2 {
+	LT left;
+	RT right;
+	das_ordered2(LT l, RT r) : left(static_cast<LT &&>(l)), right(static_cast<RT &&>(r)) {}
+};
+template <typename LT, typename RT>
+das_ordered2(LT, RT) -> das_ordered2<LT, RT>;
 
 template <typename TT, uint64_t mnh>
 inline TT &das_global(Context *c)
@@ -427,39 +711,293 @@ inline TT &das_global(Context *c)
 }
 
 template <typename TT>
-inline TT *das_ref(Context *, const TT &r) { return &const_cast<TT &>(r); }
+inline TT *das_ref(Context *, const TT &r)
+{
+	return &const_cast<TT &>(r);
+}
 
 template <typename TT>
-inline void das_zero(TT &a) { memset(&a, 0, sizeof(TT)); }
+inline TT &das_deref(Context *ctx, const TT *ptr, const char *file = "", int line = 0)
+{
+	if (!ptr) ctx->throw_error_ex("dereferencing null pointer at %s:%d", file, line);
+	return *const_cast<TT *>(ptr);
+}
+
+template <typename TT>
+inline void das_zero(TT &a)
+{
+	memset(&a, 0, sizeof(TT));
+}
 
 template <typename TT, typename QQ>
-inline void das_copy(TT &a, const QQ b) { memcpy(&a, &b, sizeof(TT)); }
+inline void das_move(TT &a, const QQ &b)
+{
+	if ((void *)&a != (void *)&b) {
+		memcpy(&a, &b, sizeof(TT));
+		memset((void *)&b, 0, sizeof(TT));
+	}
+}
 
 template <typename TT, typename QQ>
-inline bool das_nequ_val(TT a, QQ b) { return a != b; }
+inline void das_copy(TT &a, const QQ b)
+{
+	memcpy(&a, &b, sizeof(TT));
+}
+
+template <typename TT, typename QQ>
+inline bool das_nequ_val(TT a, QQ b)
+{
+	return a != b;
+}
+
+inline void das_memcpy(void *left, const void *right, int size) { memcpy(left, right, size_t(size)); }
+
+template <typename TT>
+struct das_arg {
+	static TT &pass(TT &&a) { return *((TT *)&a); }
+	static TT &pass(TT &a) { return a; }
+	static const TT &pass(const TT &&a) { return *((const TT *)&a); }
+	static const TT &pass(const TT &a) { return a; }
+};
+
+template <typename TT>
+struct das_cast {
+	template <typename QQ>
+	static TT &cast(const QQ &expr)
+	{
+		return reinterpret_cast<TT &>(const_cast<QQ &>(expr));
+	}
+};
+template <typename TT>
+struct das_cast<const TT> : das_cast<TT> {};
 
 template <typename TT>
 struct das_reinterpret {
 	template <typename QQ>
-	static TT &pass(QQ &&a) { return *((TT *)&a); }
+	static TT &pass(QQ &&a)
+	{
+		return *((TT *)&a);
+	}
 	template <typename QQ>
-	static TT &pass(QQ &a) { return *((TT *)&a); }
+	static TT &pass(QQ &a)
+	{
+		return *((TT *)&a);
+	}
 	template <typename QQ>
-	static const TT &pass(const QQ &a) { return *((const TT *)&a); }
+	static const TT &pass(const QQ &a)
+	{
+		return *((const TT *)&a);
+	}
 };
 template <typename TT>
 struct das_reinterpret<const TT> : das_reinterpret<TT> {};
 
 template <typename TT>
+struct das_iterator;
+
+template <>
+struct das_iterator<range> {
+	explicit das_iterator(const range &r) : that(r) {}
+	bool first(Context *, int32_t &i)
+	{
+		i = that.from;
+		return i < that.to;
+	}
+	bool next(Context *, int32_t &i)
+	{
+		i++;
+		return i != that.to;
+	}
+	void close(Context *, int32_t &) {}
+	range that;
+};
+
+template <typename TT>
+struct das_iterator<TArray<TT> const> {
+	explicit das_iterator(const TArray<TT> &r)
+	{
+		that = &r;
+		array_end = (const TT *)(r.data + r.size * sizeof(TT));
+	}
+	template <typename QQ>
+	bool first(Context *ctx, const QQ *&i)
+	{
+		context = ctx;
+		array_lock(*ctx, *(Array *)that, nullptr);
+		i = (const QQ *)that->data;
+		return i != (const QQ *)array_end;
+	}
+	template <typename QQ>
+	bool next(Context *, const QQ *&i)
+	{
+		i++;
+		return i != (const QQ *)array_end;
+	}
+	template <typename QQ>
+	void close(Context *ctx, const QQ *&i)
+	{
+		context = nullptr;
+		array_unlock(*ctx, *(Array *)that, nullptr);
+		i = nullptr;
+	}
+	const Array *that = nullptr;
+	const TT *array_end = nullptr;
+	Context *context = nullptr;
+};
+
+template <typename TT, typename AT, bool moveIt = false>
+struct das_ascend {
+	static TT *make(Context *ctx, TypeInfo *typeInfo, const AT &init)
+	{
+		auto size = sizeof(AT) + (typeInfo ? 16 : 0);
+		char *ptr = ctx->allocate(uint32_t(size));
+		if (typeInfo) {
+			*((TypeInfo **)ptr) = typeInfo;
+			ptr += 16;
+		}
+		memcpy(ptr, &init, sizeof(AT));
+		if (moveIt) memset((char *)&init, 0, sizeof(AT));
+		return (TT *)ptr;
+	}
+};
+
+template <typename AT>
+struct das_ascend<Lambda, AT, false> {
+	static Lambda make(Context *ctx, TypeInfo *typeInfo, const AT &init)
+	{
+		auto size = sizeof(AT) + (typeInfo ? 16 : 0);
+		char *ptr = ctx->allocate(uint32_t(size));
+		if (typeInfo) {
+			*((TypeInfo **)ptr) = typeInfo;
+			ptr += 16;
+		}
+		memcpy(ptr, &init, sizeof(AT));
+		return Lambda(ptr);
+	}
+};
+
+template <typename TT>
+struct das_delete_lambda_struct;
+
+template <typename TT>
+struct das_delete_lambda_struct<TT *> {
+	static void clear(Context *ctx, TT *ptr)
+	{
+		ctx->free(((char *)ptr) - 16, uint32_t(sizeof(TT) + 16));
+	}
+};
+
+template <typename T>
+struct SimPolicy {
+	static T Div(T a, T b, Context &, LineInfo *) { return a / b; }
+	static T Mod(T a, T b, Context &, LineInfo *) { return a % b; }
+};
+
+inline uint32_t uint32_clz(uint32_t x) { return x ? uint32_t(__builtin_clz(x)) : 32u; }
+
+template <typename ResType, int methodOffset>
+struct das_invoke_method {
+	template <typename FirstArgType, typename... ArgType>
+	static ResType invoke(Context *ctx, LineInfo *at, const FirstArgType &blk, ArgType... arg)
+	{
+		SimFunction *simFunc = ((Func *)((char *)&blk + methodOffset))->PTR;
+		if (!simFunc) ctx->throw_error_at(at, "invoke null function");
+		using fnPtrType = ResType (*)(Context *, const FirstArgType &, ArgType...);
+		return ((fnPtrType)simFunc->aotFunction)(ctx, blk, static_cast<ArgType &&>(arg)...);
+	}
+	template <typename FirstArgType>
+	static ResType invoke(Context *ctx, LineInfo *at, const FirstArgType &blk)
+	{
+		SimFunction *simFunc = ((Func *)((char *)&blk + methodOffset))->PTR;
+		if (!simFunc) ctx->throw_error_at(at, "invoke null function");
+		using fnPtrType = ResType (*)(Context *, const FirstArgType &);
+		return ((fnPtrType)simFunc->aotFunction)(ctx, blk);
+	}
+	template <typename FirstArgType, typename... ArgType>
+	static ResType invoke_cmres(Context *ctx, LineInfo *at, const FirstArgType &blk, ArgType... arg)
+	{
+		return invoke<FirstArgType, ArgType...>(ctx, at, blk, static_cast<ArgType &&>(arg)...);
+	}
+	template <typename FirstArgType>
+	static ResType invoke_cmres(Context *ctx, LineInfo *at, const FirstArgType &blk)
+	{
+		return invoke<FirstArgType>(ctx, at, blk);
+	}
+};
+
+template <int methodOffset>
+struct das_invoke_method<void, methodOffset> {
+	template <typename FirstArgType, typename... ArgType>
+	static void invoke(Context *ctx, LineInfo *at, const FirstArgType &blk, ArgType... arg)
+	{
+		SimFunction *simFunc = ((Func *)((char *)&blk + methodOffset))->PTR;
+		if (!simFunc) ctx->throw_error_at(at, "invoke null function");
+		using fnPtrType = void (*)(Context *, const FirstArgType &, ArgType...);
+		((fnPtrType)simFunc->aotFunction)(ctx, blk, static_cast<ArgType &&>(arg)...);
+	}
+	template <typename FirstArgType>
+	static void invoke(Context *ctx, LineInfo *at, const FirstArgType &blk)
+	{
+		SimFunction *simFunc = ((Func *)((char *)&blk + methodOffset))->PTR;
+		if (!simFunc) ctx->throw_error_at(at, "invoke null function");
+		using fnPtrType = void (*)(Context *, const FirstArgType &);
+		((fnPtrType)simFunc->aotFunction)(ctx, blk);
+	}
+};
+
+template <typename ResType>
+struct das_invoke_lambda {
+	template <typename... ArgType>
+	static ResType invoke(Context *ctx, LineInfo *at, const Lambda &blk, ArgType... arg)
+	{
+		SimFunction **fnpp = (SimFunction **)blk.capture;
+		if (!fnpp) ctx->throw_error_at(at, "invoke null lambda");
+		SimFunction *simFunc = *fnpp;
+		if (!simFunc) ctx->throw_error_at(at, "invoke null function");
+		using fnPtrType = ResType (*)(Context *, void *, ArgType...);
+		return ((fnPtrType)simFunc->aotFunction)(ctx, blk.capture, static_cast<ArgType &&>(arg)...);
+	}
+};
+
+struct SimNode_AotInteropBase : SimNode_CallBase {
+	SimNode_AotInteropBase() : SimNode_CallBase(LineInfo(), "") {}
+	vec4f *argumentValues = nullptr;
+};
+
+template <int argumentCount>
+struct SimNode_AotInterop : SimNode_AotInteropBase {
+	template <typename... VI>
+	SimNode_AotInterop(TypeInfo **tinfo, VI... args)
+	{
+		nArguments = argumentCount;
+		types = tinfo;
+		argumentValues = argValues;
+		vec4f argsE[argumentCount] = { args... };
+		for (int i = 0; i != argumentCount; ++i) argumentValues[i] = argsE[i];
+	}
+	vec4f argValues[argumentCount];
+};
+
+inline char *das_string_builder_temp(Context *, const SimNode_AotInteropBase &node)
+{
+	static char buf[32];
+	if (node.nArguments >= 1 && node.types && node.types[0] && node.types[0]->type == Type::tInt)
+		snprintk(buf, sizeof(buf), "%d", node.argumentValues[0].i[0]);
+	else
+		buf[0] = 0;
+	return buf;
+}
+
+template <typename TT>
 struct cast {
-	static vec4f from(TT v)
+	static vec4f from(const TT &v)
 	{
 		vec4f r;
 		memset(&r, 0, sizeof(r));
-		r.i[0] = (int32_t)v;
+		memcpy(&r, &v, sizeof(TT) < sizeof(r) ? sizeof(TT) : sizeof(r));
 		return r;
 	}
-	static TT to(vec4f x) { return *(TT *)&x; }
+	static TT to(vec4f x) { return *reinterpret_cast<TT *>(&x); }
 };
 
 template <typename TT>
--- projects/app/src/main.cpp
+++ projects/app/src/main.cpp
@@ -6,5 +6,6 @@ int main(void)
 	das::main::Standalone ctx;
 	das::main::g_ctx = &ctx;
 	ctx.main();
+	k_sleep(K_FOREVER);
 	return 0;
 }
